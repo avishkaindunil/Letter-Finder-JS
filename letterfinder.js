d1266d36ebb94f536cc4c5cadc4cad0c4bdc2532
avishkaindunil <118374198+avishkaindunil@users.noreply.github.com>
2023-06-02 23:46:02 +0530

    Update letterfinder.js

--- letterfinder.js
+++ letterfinder.js
@@ -2,7 +2,7 @@ function letterFinder(word, match) {
     var word, match;
     for(var i=0; i<word.length; i++){
         if(word[i]==match){
-            console.log("Found the", match, "at", i);
+            console.log("Founded the", match, "at", i);
         }
         else{
             console.log("---No match found at", i);
